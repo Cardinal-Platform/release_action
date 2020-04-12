ebc195a39b2e1b7514bac0902928c6eb04a88185
John Wu <524306184@qq.com>
2020-04-12 15:33:10 +0800

    fix: static build

--- build.sh
+++ build.sh
@@ -20,4 +20,4 @@ if [ $GOOS == 'windows' ]; then
   EXT='.exe'
 fi
 
-go build -ldflags "-w -s" -v -o "${PROJECT_NAME}${EXT}"
+go build -ldflags '-w -s -extldflags "-static"' -v -o "${PROJECT_NAME}${EXT}"
